33ad648eeb3209a601afb9a303d19ad1ae567a03
Prabhupada Publications <ppublications@Prabhupadas-MacBook-Air-2.local>
2026-01-02 18:59:57 +0530

    fix: combine astro configs and add site/base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    markdown: {
+  site: 'https://srigaurangasamaj108-svg.github.io',
+  base: '/vedic-site',
+  markdown: {
     syntaxHighlight: false,
   },
 });
